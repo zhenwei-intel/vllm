ac5a96e582621faff0a058513311a80d298db670
Yan Ma <yanma1@habana.ai>
2025-04-22 13:50:16 +0800

    fix gemma2 sliding window (#153)
    Signed-off-by: yan <yanma1@intel.com>

--- vllm/model_executor/models/gemma2.py
+++ vllm/model_executor/models/gemma2.py
@@ -147,10 +147,7 @@ class Gemma2Attention(nn.Module):
         # reference:
         # https://github.com/huggingface/transformers/blob/54be2d7ae87e873482b984cc956e165ca4dc0ba3/src/transformers/models/gemma2/modeling_gemma2.py#L312 # noqa
         layer_idx = extract_layer_index(prefix)
-        use_sliding_window = (layer_idx % 2 == 0 and getattr(
-            config, "interleaved_sliding_window", None) is not None)
-        sliding_window = config.interleaved_sliding_window if \
-            use_sliding_window else None
+        sliding_window = config.sliding_window if not bool(layer_idx % 2) else None
         self.attn = Attention(self.num_heads,
                               self.head_dim,
                               self.scaling,
