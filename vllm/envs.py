4f118033d901824f4dc55764cd6d0a31c3066ca1
Juncheng Gu <juncgu@gmail.com>
2025-05-14 04:28:33 +0000

    move VLLM_MULTIPROC_EXECUTE_MODEL_TIMEOUT_S to envs
    Signed-off-by: Juncheng Gu <juncgu@gmail.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -115,6 +115,7 @@ if TYPE_CHECKING:
     VLLM_ALLOW_INSECURE_SERIALIZATION: bool = False
     VLLM_NIXL_SIDE_CHANNEL_HOST: str = "localhost"
     VLLM_NIXL_SIDE_CHANNEL_PORT: int = 5557
+    VLLM_MULTIPROC_EXECUTE_MODEL_TIMEOUT_S: int = 40
 
 
 def get_default_cache_root():
@@ -764,6 +765,10 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # Port used for NIXL handshake between remote agents.
     "VLLM_NIXL_SIDE_CHANNEL_PORT":
     lambda: int(os.getenv("VLLM_NIXL_SIDE_CHANNEL_PORT", "5557")),
+
+    # Timeout for calling execute_model() in multiproc_executor
+    "VLLM_MULTIPROC_EXECUTE_MODEL_TIMEOUT_S":
+    lambda: int(os.getenv("VLLM_MULTIPROC_EXECUTE_MODEL_TIMEOUT_S", 40)),
 }
 
 # end-env-vars-definition
