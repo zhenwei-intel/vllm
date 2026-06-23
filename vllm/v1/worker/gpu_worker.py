1b8eb1129f8f1cb1df75abc0b8ecc9d6ef02ce38
liuzhenwei <zhenweiliu@habana.ai>
2026-06-23 12:30:54 +0800

    profile xpu graph memory and enable break-graph (#197)
    Signed-off-by: zhenwei-intel <zhenwei.liu@intel.com>
    (cherry picked from commit c36f3884570ef244eace5dc90655508c5590fb06)
    
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>
    (cherry picked from commit 1a0837d70ef938f2231d997fffe13527d0913ca1)

--- vllm/v1/worker/gpu_worker.py
+++ vllm/v1/worker/gpu_worker.py
@@ -526,9 +526,8 @@ class Worker(WorkerBase):
         # XPU stays excluded (see #39977).
         cudagraph_memory_estimate = 0
         if (
-            current_platform.is_cuda_alike()
-            and self.vllm_config.compilation_config.cudagraph_mode != CUDAGraphMode.NONE
-        ):
+            current_platform.is_cuda_alike() or current_platform.is_xpu()
+        ) and self.vllm_config.compilation_config.cudagraph_mode != CUDAGraphMode.NONE:
             cudagraph_memory_estimate = self.model_runner.profile_cudagraph_memory()
 
         # Respect the opt-in flag as originally designed.
