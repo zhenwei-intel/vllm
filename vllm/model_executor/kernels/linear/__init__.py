0d4a79d0bcfce6d6686cab7f6e1263cdeec0d306
Zhefeng, Qiao <zhefeng.qiao@intel.com>
2026-08-26 23:20:01 +0800

    Set XPUW8A8FP8LinearKernel as default (#251)
    Signed-off-by: Qiao, Zhefeng <zhefeng.qiao@intel.com>

--- vllm/model_executor/kernels/linear/__init__.py
+++ vllm/model_executor/kernels/linear/__init__.py
@@ -418,8 +418,8 @@ _POSSIBLE_FP8_KERNELS: dict[PlatformEnum, list[type[FP8ScaledMMLinearKernel]]] =
         ChannelWiseTorchFP8ScaledMMLinearKernel,
     ],
     PlatformEnum.XPU: [
-        XPUW8A16FP8LinearKernel,
         XPUW8A8FP8LinearKernel,
+        XPUW8A16FP8LinearKernel,
         PerTensorTorchFP8ScaledMMLinearKernel,
         ChannelWiseTorchFP8ScaledMMLinearKernel,
     ],
